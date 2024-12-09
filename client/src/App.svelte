317659f6722408c464e1a66ad3b7b432284c9ef4
Antonis Pishias <pishias92@gmail.com>
2024-12-09 12:40:25 +0000

    track layout

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,13 +1,14 @@
 <script>
   import { onMount } from 'svelte';
-  const Pages = Object.freeze({
+  const Details = Object.freeze({
     Main: 'main',
-    Artist: 'artist',
-    Label: 'label',
+    ArtistTop10: 'at10',
+    ArtistLabels: 'al',
+    ArtistTracks: 'all',
   })
+
   let loading = false;
 
-  // let page = Pages.Main;
   let artists = null;
   let labels = null;
   let searchTerm = "John Summit";
@@ -21,15 +22,31 @@
   let isOpenArtistTracksByLabel = false;
   let isOpenArtistTop10 = false;
 
-
   let label = null;
   let labelArtists = null;
   let labelTop10 = null;
 
-
-  // function go_to_page(p) {
-  //   page = p
-  // }
+  function toggleDetails(detail) {
+    switch (detail) {
+      case Details.Main:
+        setMainView()
+        break;
+      case Details.ArtistLabels:
+        isOpenArtistsDetail = false;
+        isOpenArtistLabelByDate = true;
+        break;   
+      case Details.ArtistTop10:
+        isOpenArtistsDetail = false;
+        isOpenArtistTop10 = true;
+        break;   
+      case Details.ArtistTracks:
+        isOpenArtistsDetail = false;
+        isOpenArtistTracksByLabel = true;
+        break;   
+      default:
+        break;
+    }
+  }
 
   function setMainView() {
     isOpenArtistsDetail = true;
@@ -39,9 +56,8 @@
   }
 
   function search() {
-    // go_to_page(Pages.Main)
     loading = true;
-    setMainView();
+    toggleDetails(Details.Main)
     artists = null;
     labels = null;
     fetch(`./search/${searchTerm}`)
@@ -70,19 +86,15 @@
         isOpenArtistLabelByDate = true;
         loading = false;
       })
-      // .then(() => go_to_page(Pages.Artist) );
   }
 
   function get_all_artists_for_label(slug, id) {
     fetch(`./label/${slug}/${id}/artists`)
-      // .then(d => d.json())
       .then(d => ( console.log(d) ));
   }
 
   onMount(() => {
 		console.log('the component has mounted');
-    // go_to_page(Pages.Main)
-
 	});
 
 </script>
@@ -94,12 +106,12 @@
   Loading......
 {/if}
 
-<!-- {#if page === Pages.Main} -->
   {#if !!artists}
   <details open={isOpenArtistsDetail} 
-          on:toggle={e => {            
-            if (e.target.open) setMainView()
-            }}>
+    on:toggle={e => {
+      if (e.target.open) 
+        toggleDetails(Details.Main)
+      }}>
     <summary><b>Artists</b></summary>
     {#each artists as artist}
       <div on:click={() => get_labels_by_date_for_artist(artist.slug, artist.id)}>
@@ -110,11 +122,7 @@
     {/each}
   </details>
   {/if}
-<!-- {/if} -->
 
-
-
-<!-- {#if page === Pages.Artist} -->
 
   {#if !!artist}
   <h1>Artist</h1>
@@ -126,12 +134,10 @@
   </div>
 
   <details open={isOpenArtistLabelByDate} 
-          on:toggle={e => {
-            if (e.target.open) {
-              isOpenArtistsDetail = false;
-              isOpenArtistLabelByDate = true;
-            }
-            }}>
+  on:toggle={e => {
+    if (e.target.open) 
+      toggleDetails(Details.ArtistLabels)
+    }}>
     <summary><b>Labels By Date</b></summary>
     {#each artistLabelByDate as labelItem}
       <!-- <div on:click={() => get_labels(artist.slug, artist.id)}> -->
@@ -143,49 +149,48 @@
     {/each}
   </details>
 
-  <details open={isOpenArtistTop10} 
-          on:toggle={e => {
-            if (e.target.open)
-            {
-              isOpenArtistsDetail = false;
-              isOpenArtistTop10 = true;
-            }
-            }}>
+  <details class="detail" id='artist-top-10' open={isOpenArtistTop10} 
+    on:toggle={e => {
+      if (e.target.open) 
+        toggleDetails(Details.ArtistTop10)
+      }}>
     <summary><b>Top 10 Tracks</b></summary>
+    <div class="tracks flex">
+
     {#each artistTop10 as track}
     <!-- <div on:click={() => get_labels(artist.slug, artist.id)}> -->
-    <div>  
-      <h4>{track.name}</h4>
-      <div>Artists</div>
-      {#each track.artists as artist}
-      <div>
-        <img width="36" src="{artist.image}" alt="{artist.slug}-img"> 
-        <span>{artist.name}</span>
-      </div>
-      {/each}
-      {#if !!track.remixers}
-        <div>Remixed By</div>
-        {#each track.remixers as artist}
-        <div>
-          <img width="36" src="{artist.image}" alt="{artist.slug}-img"> 
-          <span>{artist.name}</span>
+    <div class="track">  
+        <img width="128" src="{track.image}" alt="{track.slug}-img"> 
+        <audio width="320" height="240" src="{track.sample}" controls> </audio>
+        <div class="track-info">
+          <h4>{track.name}</h4>
+          <div class="title">Artists</div>
+          {#each track.artists as artist}
+          <div>
+            <div><img width="48" src="{artist.image}" alt="{artist.slug}-img"> </div>
+            <div>{artist.name}</div>
+          </div>
+          {/each}
+          {#if track.remixers.length > 0}
+          <div class="title">Remixed By</div>
+            {#each track.remixers as artist}
+            <div>
+              <div><img width="48" src="{artist.image}" alt="{artist.slug}-img"> </div>
+              <div>{artist.name}</div>
+            </div>
+            {/each}
+          {/if}
         </div>
-        {/each}
-      {/if}
-      <br>
-      <img width="128" src="{track.image}" alt="{track.slug}-img"> 
-      <audio width="320" height="240" src="{track.sample}" controls> </audio>
+      </div>
+    {/each}
     </div>
-  {/each}
   </details>
 
   <details open={isOpenArtistTracksByLabel} 
-          on:toggle={e => {
-            if (e.target.open) {
-              isOpenArtistsDetail = false;
-              isOpenArtistTracksByLabel = true;
-            }
-            }}>
+    on:toggle={e => {
+      if (e.target.open) 
+        toggleDetails(Details.ArtistTracks)
+      }}>
   <summary><b>All Tracks By Date</b></summary>
   {#each artistTracksByLabel as tracksByLabel}
     <!-- <div on:click={() => get_labels(artist.slug, artist.id)}> -->
@@ -193,38 +198,36 @@
       <h4>{tracksByLabel.label.name}</h4>
       <img width="128" src="{tracksByLabel.label.image}" alt="{tracksByLabel.label.slug}-img"> 
     </div>
-    {#each tracksByLabel.tracks as track}
+
+    <div class="tracks flex">
+      {#each tracksByLabel.tracks as track}
       <!-- <div on:click={() => get_labels(artist.slug, artist.id)}> -->
-      <div>  
-        <h4>{track.name}</h4>
-        <div>Artists</div>
-        {#each track.artists as artist}
-          <img width="36" src="{artist.image}" alt="{artist.slug}-img"> 
-          <span>{artist.name}</span>
-        {/each}
-        <div>Remixed By</div>
-        {#each track.remixers as artist}
-          <img width="36" src="{artist.image}" alt="{artist.slug}-img"> 
-          <span>{artist.name}</span>
-        {/each}
-        <br>
-        <img width="128" src="{track.image}" alt="{track.slug}-img"> 
-        <audio width="320" height="240" src="{track.sample}" controls> </audio>
+      <div class="track">  
+          <img width="128" src="{track.image}" alt="{track.slug}-img"> 
+          <audio width="320" height="240" src="{track.sample}" controls> </audio>
+          <div class="track-info">
+            <h4>{track.name}</h4>
+            <div class="title">Artists</div>
+            {#each track.artists as artist}
+            <div>
+              <div><img width="48" src="{artist.image}" alt="{artist.slug}-img"> </div>
+              <div>{artist.name}</div>
+            </div>
+            {/each}
+            {#if track.remixers.length > 0}
+            <div class="title">Remixed By</div>
+              {#each track.remixers as artist}
+              <div>
+                <div><img width="48" src="{artist.image}" alt="{artist.slug}-img"> </div>
+                <div>{artist.name}</div>
+              </div>
+              {/each}
+            {/if}
+          </div>
+        </div>
+      {/each}
       </div>
-    {/each}
   {/each}
   </details>
 
   {/if}
-<!-- {/if} -->
-
-
-
-<style>
-
-details {
-  padding: 12px; 
-  border: 12px solid #efefef;
-  border-radius: 5px;
-}
-</style>
